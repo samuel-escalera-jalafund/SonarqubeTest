10d18f07563d538573f1efbac3848d1bd65d419f
Samuel Escalera <Samuel.Escalera@fundacion-jala.org>
2024-10-02 00:47:51 -0400

    Test commit

---
+++ src/main/java/org/example/HonorSmartphone.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class HonorSmartphone {
+
+    public String model;
+
+    public HonorSmartphone(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
---
+++ src/main/java/org/example/SamsungSmartphone.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class SamsungSmartphone {
+
+    public String model;
+
+    public SamsungSmartphone(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
